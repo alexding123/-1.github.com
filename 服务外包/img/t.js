536f9ce526c7f3c424bbb3c7e0050165280d4c13
alexding123 <104618001+alexding123@users.noreply.github.com>
2022-06-23 10:34:23 +0800

    Add files via upload

---
+++ 服务外包/img/t.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2022-06-22 12:35:29
+ * @version $Id$
+ */
+function showPopup(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+  }
+function hidePopup(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+ }
